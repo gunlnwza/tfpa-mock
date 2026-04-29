c58da01b097e459bb65efd86391cd0cd5f64e1e8
Natachat Teecharcharn <nteechar@Natachats-MacBook-Air.local>
2026-04-29 20:13:51 +0700

    feat: Hero image full size

--- frontend/src/features/Hero.tsx
+++ frontend/src/features/Hero.tsx
@@ -2,9 +2,34 @@ import handshakeImg from "../assets/stock/handshake.png"
 import { PrimaryCTAButton } from "../components/ui/Button";
 
 
-export function Hero() {
+function Card() {
   return (
-    <div className="relative h-[500px] w-full">
+    <div className={`
+      md:text-5xl font-bold leading-tight
+      space-y-2 p-8 mb-30
+      bg-white/95 rounded-xl text-center shadow-lg
+    `}>
+      <h1 className="mb-6">
+        <span className="block text-black text-3xl mb-4">
+          CFP® Professionals Deliver
+        </span>
+        <span className="block text-blue-800 text-5xl mb-6">
+          "The Standard of Excellence"
+        </span>
+        <span className="block text-yellow-600 text-3xl mb-4">
+          เป้าหมายชีวิตสำเร็จได้ด้วยนักวางแผนการเงิน CFP®
+        </span>
+      </h1>
+      <div className="text-xl">
+        <PrimaryCTAButton to="find_planner">รับบริการวางแผนการเงิน</PrimaryCTAButton>
+      </div>
+    </div>
+  );
+}
+
+export function Hero({ className }: { className: string }) {
+  return (
+    <div className={`relative w-full ${className} flex justify-center items-center`}>
       {/* background image */}
       <img
         src={handshakeImg}
@@ -16,23 +41,8 @@ export function Hero() {
       <div className="absolute inset-0 bg-black/10" />
 
       {/* text */}
-      <div className="relative z-10 h-full flex flex-col justify-center p-8">
-        <div className="md:text-5xl font-bold leading-tight space-y-2 bg-white/90 rounded-xl p-4">
-          <h1 className="mb-6">
-            <span className="block text-black text-3xl mb-4">
-              CFP® Professionals Deliver
-            </span>
-            <span className="block text-blue-800 text-5xl mb-6">
-              "The Standard of Excellence"
-            </span>
-            <span className="block text-yellow-600 text-3xl mb-4">
-              เป้าหมายชีวิตสำเร็จได้ด้วยนักวางแผนการเงิน CFP®
-            </span>
-          </h1>
-          <div className="text-xl">
-            <PrimaryCTAButton to="find_planner">รับบริการวางแผนการเงิน</PrimaryCTAButton>
-          </div>
-        </div>
+      <div className="relative">
+        <Card />
       </div>
     </div>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,14 +5,22 @@ import { CompanyMarquee } from "../features/CompanyMarquee";
 import { SecondaryStrip } from "../features/SecondaryStrip";
 import { Announcements } from "../features/Announcements";
 
+import { Header } from "../components/layout/global/Header";
+import { Footer } from "../components/layout/global/Footer";
 
 export default function Home() {
   return (
-    <Page padding={false} >
-      <Hero />
+    <div className="font-sans">
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <Hero className="flex-1" />
+      </div>
+
       <CompanyMarquee />
       <SecondaryStrip />
       <Announcements />
-    </Page>
+
+      <Footer />
+    </div>
   );
 }
